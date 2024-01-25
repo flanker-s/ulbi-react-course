a0014c31cd7192f618b0fd5e413385c728d320ff
gradient-mind <mohavkv12@gmail.com>
2024-01-25 17:28:10 +0300

    Ref. Extract router and router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import "./styles/index.scss";
 import "./styles/reset.scss";
-import { Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import { useTheme } from "@/app/providers/ThemeProvider";
 import { classNames } from "@/shared/classNames/classNames";
-import { MainPage } from "@/pages/MainPage";
-import { AboutPage } from "@/pages/AboutPage";
+import { AppRouter } from "@/app/providers/AppRouter";
 
 const App = () => {
 
@@ -17,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle Theme</button><br />
             <Link to={'/'}>Главная</Link><br />
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/" element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 }
