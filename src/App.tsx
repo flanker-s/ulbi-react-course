032c7bcfb2578132535a880de7354efe5d5e881a
gradient-mind <mohavkv12@gmail.com>
2024-01-25 10:36:49 +0300

    Add. Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import "./style.scss";
+import "./styles/index.scss";
+import "./styles/reset.scss";
 import { Route, Routes } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Link } from "react-router-dom";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle Theme</button><br />
             <Link to={'/'}>Главная</Link><br />
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
